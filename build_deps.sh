99a76e81e04d2b0a832efac60e7d5f27ecb298e1
Lauri Räsänen <hello@rasanen.dev>
2026-06-28 02:52:22 +0300

    Add slang and KTX deps

---
+++ build_deps.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Build CMake dependencies that Meson doesn't like.
+#
+
+set -euo pipefail
+
+ROOT=$(pwd)
+
+git submodule update --init --recursive
+
+echo "BUILDING KTXLIB"
+rm -rf "$ROOT/thirdparty/KTX-Software/lib/build"
+cd "$ROOT/thirdparty/KTX-Software/lib"
+
+git checkout -- .
+
+cmake . -B build \
+    -DLIBKTX_VERSION_READ_ONLY=OFF \
+    -DLIBKTX_VERSION_FULL=ON \
+    -DLIBKTX_FEATURE_KTX1=ON \
+    -DLIBKTX_FEATURE_KTX2=ON \
+    -DLIBKTX_FEATURE_VK_UPLOAD=ON \
+    -DLIBKTX_FEATURE_GL_UPLOAD=OFF \
+    -DLIBKTX_FEATURE_ETC_UNPACK=OFF
+
+cmake --build build
+
+echo "BUILDING SLANG"
+rm -rf "$ROOT/thirdparty/slang/buid"
+cd "$ROOT/thirdparty/slang"
+
+# needed for lib naming scheme
+git fetch https://github.com/shader-slang/slang.git 'refs/tags/*:refs/tags/*'
+
+cmake --preset default
+cmake --build --preset releaseWithDebugInfo
