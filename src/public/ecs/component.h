b0d4ba5a5d124a90021febaeed2978889618cdeb
Lauri Räsänen <hello@rasanen.dev>
2026-07-07 20:02:58 +0300

    ecs: add basic types

---
+++ src/public/ecs/component.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <cstddef>
+
+namespace yar
+{
+class Entity;
+
+class ComponentTypeIDSystem
+{
+  private:
+    static size_t nextTypeID;
+
+  public:
+    template<typename T>
+    static size_t GetTypeID()
+    {
+        static size_t typeID = nextTypeID++;
+        return typeID;
+    }
+};
+
+size_t ComponentTypeIDSystem::nextTypeID = 0;
+
+class Component
+{
+  public:
+    enum class State
+    {
+        Uninitialized,
+        Initializing,
+        Active,
+        Destroying,
+        Destroyed
+    };
+
+    Component() = default;
+
+    virtual ~Component()
+    {
+        if (m_state != State::Destroyed)
+        {
+            m_state = State::Destroying;
+            OnDestroy();
+            m_state = State::Destroyed;
+        }
+    }
+
+    Component(const Component&)            = default;
+    Component(Component&&)                 = default;
+    Component& operator=(const Component&) = default;
+    Component& operator=(Component&&)      = default;
+
+    virtual void Initialize()
+    {
+        if (m_state == State::Uninitialized)
+        {
+            m_state = State::Initializing;
+            OnInitialize();
+            m_state = State::Active;
+        }
+    }
+
+    void Destroy()
+    {
+        if (m_state == State::Active)
+        {
+            m_state = State::Destroying;
+            OnDestroy();
+            m_state = State::Destroyed;
+        }
+    }
+
+    bool IsActive() const
+    {
+        return m_state == State::Active;
+    }
+
+    void SetOwner(Entity* entity)
+    {
+        m_owner = entity;
+    }
+
+    Entity* GetOwner() const
+    {
+        return m_owner;
+    }
+
+    template<typename T>
+    static size_t GetTypeID()
+    {
+        return ComponentTypeIDSystem::GetTypeID<T>();
+    }
+
+  protected:
+    virtual void OnInitialize()
+    {
+    }
+
+    virtual void OnDestroy()
+    {
+    }
+
+    virtual void Update(float deltaTime)
+    {
+    }
+
+    virtual void FixedUpdate(float deltaTime)
+    {
+    }
+
+    virtual void Render()
+    {
+    }
+
+    Entity* m_owner;
+    State   m_state;
+
+    friend class Entity;
+};
+}; // namespace yar
---
+++ src/public/ecs/entity.h
@@ -0,0 +1,154 @@
+#pragma once
+
+#include "component.h"
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+namespace yar
+{
+class Entity
+{
+  public:
+    explicit Entity(const std::string& name) : m_name(name)
+    {
+    }
+
+    Entity()          = delete;
+    virtual ~Entity() = default;
+
+    Entity(const Entity&)            = default;
+    Entity(Entity&&)                 = default;
+    Entity& operator=(const Entity&) = default;
+    Entity& operator=(Entity&&)      = default;
+
+    void Initialize()
+    {
+        for (auto& comp : m_components)
+        {
+            comp->Initialize();
+        }
+    }
+
+    void Update(float deltaTime)
+    {
+        if (!m_active)
+        {
+            return;
+        }
+
+        for (auto& comp : m_components)
+        {
+            comp->Update(deltaTime);
+        }
+    }
+
+    void FixedUpdate(float deltaTime)
+    {
+        if (!m_active)
+        {
+            return;
+        }
+
+        for (auto& comp : m_components)
+        {
+            comp->FixedUpdate(deltaTime);
+        }
+    }
+
+    void Render()
+    {
+        if (!m_active)
+        {
+            return;
+        }
+
+        for (auto& comp : m_components)
+        {
+            comp->Render();
+        }
+    }
+
+    const std::string& GetName() const
+    {
+        return m_name;
+    }
+
+    bool IsActive() const
+    {
+        return m_active;
+    }
+
+    void SetActive(bool active)
+    {
+        m_active = active;
+    }
+
+    template<typename T, typename... Args>
+    T* AddComponent(Args&&... args)
+    {
+        static_assert(std::is_base_of<Component, T>::value, "T must derive from Component");
+
+        size_t typeID = Component::GetTypeID<T>();
+
+        auto it = m_componentMap.find(typeID);
+        if (it != m_componentMap.end())
+        {
+            return static_cast<T*>(it->second);
+        }
+
+        auto component         = std::make_unique<T>(std::forward<Args>(args)...);
+        T*   componentPtr      = component.get();
+        m_componentMap[typeID] = componentPtr;
+        m_components.push_back(std::move(component));
+        return componentPtr;
+    }
+
+    template<typename T>
+    T* GetComponent()
+    {
+        static_assert(std::is_base_of<Component, T>::value, "T must derive from Component");
+
+        size_t typeID = Component::GetTypeID<T>();
+        auto   it     = m_componentMap.find(typeID);
+        if (it != m_componentMap.end())
+        {
+            return static_cast<T*>(it->second);
+        }
+        return nullptr;
+    }
+
+    template<typename T>
+    bool RemoveComponent()
+    {
+        static_assert(std::is_base_of<Component, T>::value, "T must derive from Component");
+
+        size_t typeID = Component::GetTypeID<T>();
+        auto   it     = m_componentMap.find(typeID);
+        if (it != m_componentMap.end())
+        {
+            Component* componentPtr = it->second;
+            m_componentMap.erase(it);
+
+            for (auto compIt = m_components.begin(); compIt != m_components.end(); ++compIt)
+            {
+                if (compIt->get() == componentPtr)
+                {
+                    m_components.erase(compIt);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+  private:
+    std::string m_name;
+
+    bool m_active = true;
+
+    std::vector<std::unique_ptr<Component>> m_components;
+    std::unordered_map<size_t, Component*>  m_componentMap;
+};
+}; // namespace yar
