7760f992bc327862e173453086ffd2c0fd6144c5
Lauri Räsänen <hello@rasanen.dev>
2026-07-12 00:42:37 +0300

    update scripts

--- gdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd build
-meson compile
-LSAN_OPTIONS="suppressions=../suppr.txt" gdb yar
-
--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 cd build
 meson compile
-LD_LIBRARY_PATH="/usr/local/lib" LSAN_OPTIONS="suppressions=../suppr.txt" ./yar
+LD_LIBRARY_PATH="/usr/local/lib" LSAN_OPTIONS="suppressions=../suppr.txt" ./sample
 
